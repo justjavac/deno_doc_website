529170e0908ce8691712b49b6cf6bcac1b79cfcd
迷渡 <justjavac@gmail.com>
2021-02-04 17:20:39 +0800

    翻译 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -10,9 +10,9 @@ function NotFound() {
         <title>404 not found - deno doc</title>
       </head>
       <div className="flex flex-col items-center justify-center h-full p-4 text-center">
-        <div className="text-3xl text-gray-800">404 - Page not found</div>
+        <div className="text-3xl text-gray-800">404 - 你访问的页面不存在</div>
         <Link href="/">
-          <a className="mt-4 text-xl link">Go back home</a>
+          <a className="mt-4 text-xl link">返回首页</a>
         </Link>
       </div>
     </>
